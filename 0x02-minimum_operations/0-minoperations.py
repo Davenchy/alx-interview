330eeaf6c150ce70e35bd7fe224f1a3c1320add7
Fadi Asaad <firon1222@gmail.com>
2024-04-24 01:55:42 +0200

    rewrite for debugging

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,9 +13,32 @@ def find_divider(x: int) -> int:
     return 1
 
 
-def minOperations(n: int) -> int:
+def _minOperations(n: int) -> int:
     """find the minimum copyAll and paste operations to repeat the character
     n times
+
+    Examples:
+        # copyAll > paste > copyAll > paste
+        >>> _minOperations(4)
+        4
+
+        # copyAll > paste > paste > copyAll > paste > paste
+        >>> _minOperations(9)
+        6
+
+        # copyAll > paste > paste > copyAll > paste > copyAll > paste
+        >>> _minOperations(12)
+        7"""
+    if n <= 1:
+        return 0
+
+    divider = find_divider(n)
+    return n // divider + _minOperations(divider)
+
+
+def minOperations(n: int) -> int:
+    """find the minimum copyAll and paste operations to repeat the character
+    n times.
 
     Examples:
         # copyAll > paste > copyAll > paste
@@ -28,9 +51,23 @@ def minOperations(n: int) -> int:
 
         # copyAll > paste > paste > copyAll > paste > copyAll > paste
         >>> minOperations(12)
-        7"""
+        7
+        """
     if n <= 1:
         return 0
+    elif n <= 5:
+        return n
 
-    divider = find_divider(n)
-    return n // divider + minOperations(divider)
+    if n % 2 == 0:
+        return minOperations(n // 2) + n // minOperations(n // 2)
+    else:
+        return (minOperations(n // 2) + 1) + n // (minOperations(n // 2) + 1)
+
+
+if __name__ == "__main__":
+    import doctest
+    # import sys
+    doctest.testmod()
+
+    # for i in range(10, 31):
+    #     print(i, minOperations(i), minOperations2(i))
