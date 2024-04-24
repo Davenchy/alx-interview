1db0a96afe9d9b02acedcdcdde7be24eab6e055d
Fadi Asaad <firon1222@gmail.com>
2024-04-24 02:41:21 +0200

    final update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,36 +4,14 @@
 
 def find_divider(x: int) -> int:
     """find the nearest lower number of x that is divisible by x"""
+    import math
     if x <= 3:
         return 1
 
-    for i in range(x - 1, 1, -1):
+    for i in range(int(math.sqrt(x)), 1, -1):
         if x % i == 0:
             return i
     return 1
-
-
-def _minOperations(n: int) -> int:
-    """find the minimum copyAll and paste operations to repeat the character
-    n times
-
-    Examples:
-        # copyAll > paste > copyAll > paste
-        >>> _minOperations(4)
-        4
-
-        # copyAll > paste > paste > copyAll > paste > paste
-        >>> _minOperations(9)
-        6
-
-        # copyAll > paste > paste > copyAll > paste > copyAll > paste
-        >>> _minOperations(12)
-        7"""
-    if n <= 1:
-        return 0
-
-    divider = find_divider(n)
-    return n // divider + _minOperations(divider)
 
 
 def minOperations(n: int) -> int:
@@ -58,16 +36,10 @@ def minOperations(n: int) -> int:
     elif n <= 5:
         return n
 
-    if n % 2 == 0:
-        return minOperations(n // 2) + n // minOperations(n // 2)
-    else:
-        return (minOperations(n // 2) + 1) + n // (minOperations(n // 2) + 1)
+    x = find_divider(n)
+    return (n // x) + minOperations(x)
 
 
 if __name__ == "__main__":
     import doctest
-    # import sys
     doctest.testmod()
-
-    # for i in range(10, 31):
-    #     print(i, minOperations(i), minOperations2(i))
