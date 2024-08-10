1ca4d90cc8789781139c2f47dd3277e66510ad6e
Fadi Asaad <firon1222@gmail.com>
2024-08-10 10:06:42 +0300

    update: algorithm to make it faster

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """ Solving prime game problem. """
 
-prime_memory = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+def generate_primes(n):
+    """ Generate all primes up to n. """
+    primes = []
+
+    for i in range(2, n + 1):
+        is_prime = True
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes.append(i)
+    return primes
 
 
-def is_prime(n):
-    """ Check if number is prime. """
-    if n in prime_memory:
-        return True
-    if n <= 30:
-        return False
-
-    for i in range(2, n // 3):
-        if n % i == 0:
-            return False
-
-    prime_memory.append(n)
-    return True
-
-
-def is_maria_wins(n):
+def is_maria_wins(n, primes=None):
     """ Check if maria wins a game of n. """
-    primes = [i for i in range(n + 1) if is_prime(i)]
-    return len(primes) % 2 != 0
+    if primes is None:
+        primes = generate_primes(n)
+
+    counter = 0
+    for i in primes:
+        if i > n:
+            break
+        counter += 1
+    return counter % 2 != 0
 
 
 def isWinner(x, nums):
@@ -38,11 +44,12 @@ def isWinner(x, nums):
 
     Returns the player who won the most games or None if there is a tie."""
     maria_score, ben_score = 0, 0
+    primes = generate_primes(max(nums[:x]))
 
     for i in range(x):
         n = nums[i]
 
-        if is_maria_wins(n):
+        if is_maria_wins(n, primes):
             maria_score += 1
         else:
             ben_score += 1
